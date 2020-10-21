f442ff5f7e6c44c542b7375d1f573eb93188c1b6
aryo <9f.aryosatyo@gmail.com>
2020-10-21 11:24:50 +0700

    update tantangan

--- app/src/main/java/id/ac/polinema/skor/fragments/ScoreFragment.java
+++ app/src/main/java/id/ac/polinema/skor/fragments/ScoreFragment.java
@@ -80,4 +80,26 @@ public class ScoreFragment extends Fragment {
 
 	}
 
+	public String getHomeScorer() {
+		StringBuilder result = new StringBuilder();
+		for (GoalScorer g : homeGoalScorerList) {
+			result.append(g.getName())
+					.append(" ")
+					.append(g.getMinute())
+					.append("\" ");
+		}
+		return result.toString();
+	}
+
+	public String getAwayScorer() {
+		StringBuilder result = new StringBuilder();
+		for (GoalScorer g : awayGoalScorerList) {
+			result.append(g.getName())
+					.append(" ")
+					.append(g.getMinute())
+					.append("\" ");
+		}
+		return result.toString();
+	}
+
 }
